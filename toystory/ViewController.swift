d5360e07bcb48389dbe632a1cd016cc17f868d7b
CMoney <cmoney@jianjieandemacbook-pro.local>
2018-07-25 11:14:28 +0800

    Initial Commit

---
+++ toystory/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  toystory
+//
+//  Created by CMoney on 2018/7/25.
+//  Copyright © 2018年 CMoney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
